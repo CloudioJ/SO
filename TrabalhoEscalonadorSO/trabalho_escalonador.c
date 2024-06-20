168e284c16be5e40f365af86f733f7f4c43814a4
Claudio <claudio.junior.ts2@gmail.com>
2024-06-20 14:43:28 -0300

    Basic functions working

---
+++ TrabalhoEscalonadorSO/trabalho_escalonador.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define BUFFER_SIZE 1024
+#define TRUE 1
+#define FALSE 0
+
+typedef struct Tuple {
+    int shortestIndex;
+    int shortestTime;
+} Tuple;
+
+void sjf(char processNames[][BUFFER_SIZE], int processTimes[], int lines, char *argv[]);
+Tuple findShortestIndex(int processTimes[], int lines);
+
+int main(int argc, char *argv[]){
+    char *filename =  argv[1];
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL){
+        printf("Erro ao encontrar o arquivo %s", filename);
+        return 1;
+    }
+
+    if (*argv[2] != '1' && *argv[2] != '2'){
+        printf("O segundo argumento deve ser 1 ou 2");
+        return 1;
+    }
+
+    char buffer[BUFFER_SIZE];
+    char *delimiter_chars = " ";
+    char *last_token;
+    int lines = 1;
+
+    char ch;
+
+    while((ch = getc(fp)) != EOF){
+        if (ch == '\n'){
+            ++lines;
+        }
+    }
+
+    rewind(fp);
+
+    // printf("lines: %d\n", lines);
+    char processNames[lines][BUFFER_SIZE];
+    int processTimes[lines];
+
+    int i = 0;
+    int j = 0;
+
+    while(fgets(buffer, BUFFER_SIZE, fp)){
+        last_token = strtok(buffer, delimiter_chars);
+        while(last_token != NULL){
+            if (i % 2 == 0){
+                strcpy(processNames[j], last_token);
+                // printf("%s\n", processNames[j]);    
+            } else {
+                processTimes[j] = atoi(last_token);
+                // printf("%d\n", processTimes[j]);
+                ++j;
+            }
+            last_token = strtok(NULL, delimiter_chars);
+            ++i;
+        }
+
+    }
+
+    fclose(fp);
+    sjf(processNames, processTimes, lines, argv);
+
+    return 0;
+}
+
+void sjf(char processNames[][BUFFER_SIZE], int processTimes[], int lines, char *argv[]){
+    
+
+    switch (*argv[2]){
+        case '1':
+            int startTime = 0;
+            int currentTime = 0;
+            Tuple t = {lines + 1, INT_MAX};
+
+            for(int i = 0; i < lines; ++i){
+                t = findShortestIndex(processTimes, lines);
+
+                currentTime = processTimes[t.shortestIndex];
+                processTimes[t.shortestIndex] = INT_MAX;
+                printf("%s;%d;%d\n", processNames[t.shortestIndex], startTime, currentTime + startTime);
+                startTime += t.shortestTime;
+            }
+            break;
+
+        case '2':
+            Tuple t1 = {lines + 1, INT_MAX};
+            Tuple t2 = {lines + 1, INT_MAX};
+            int startTime1 = 0;
+            int startTime2 = 0;
+            char processor1[] = "Processador_1";
+            char processor2[] = "Processador_2";
+
+            for(int i = 0; i < lines; i += 2){
+                t1 = findShortestIndex(processTimes, lines);
+
+                int currentTime1 = processTimes[t1.shortestIndex];
+                processTimes[t1.shortestIndex] = INT_MAX;
+                
+                for(int j = 0; j < lines; ++j){
+                    if (j != t1.shortestIndex){
+                        t2 = findShortestIndex(processTimes, lines);
+                        break;
+                    }
+                }
+
+                int currentTime2 = processTimes[t2.shortestIndex];
+
+                processTimes[t2.shortestIndex] = INT_MAX;
+
+                strcat(processor1, processNames[t1.shortestIndex]);
+                printf("%s;%d;%d\n", processNames[t1.shortestIndex], startTime1, currentTime1 + startTime1);
+                printf("Processador_2\n");
+                printf("%s;%d;%d\n", processNames[t2.shortestIndex], startTime2, currentTime2 + startTime2);
+
+                startTime1 += currentTime1;
+                startTime2 += currentTime2;
+            }
+        break;
+    }
+}
+
+Tuple findShortestIndex(int processTimes[], int lines){
+    Tuple time = {lines + 1, INT_MAX};
+
+    for(int i = 0; i < lines; ++i){
+        if (processTimes[i] <= time.shortestTime){
+            time.shortestIndex = i;
+            time.shortestTime = processTimes[i];
+        }
+    }
+
+    return time;
+}
